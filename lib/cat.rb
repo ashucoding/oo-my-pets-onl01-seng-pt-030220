85b0de75d48bf9700184207fe35f776ec559a522
Shana Wiltshire <shanawiltshire@gmail.com>
2020-05-03 23:57:19 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,10 @@
-Class Cat
+class Cat
+  # code goes here	  # code goes here
+end 	  attr_reader :name
+  attr_accessor :mood
 
+  def initialize(name, mood = "nervous")
+    @mood = mood
+    @name = name
+  end
 end
