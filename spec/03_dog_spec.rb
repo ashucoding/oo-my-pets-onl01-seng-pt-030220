e1bdcc3b354d222445cc49ed0e84df4e948d9ddf
Shana Wiltshire <shanawiltshire@gmail.com>
2020-05-03 23:52:25 +0000

    Automatically backed up by Learn

--- spec/03_dog_spec.rb
+++ spec/03_dog_spec.rb
@@ -1,5 +1,3 @@
-require_relative 'spec_helper.rb'
-
 describe Dog do
 
   before do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,6 @@
-require_relative '../config/environment'
+require_relative '../lib/cat'
+require_relative '../lib/dog'
+require_relative '../lib/owner'
 
 RSpec.configure do |config|
   # config here
